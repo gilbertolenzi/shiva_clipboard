b41dd587977f5fd5617d2c3a23471d0ed5c27134
gilberto lenzi <gilbertolenzi@gmail.com>
2013-02-25 14:05:18 +0000

    Add an onchange trigger to the select box

--- popup.js
+++ popup.js
@@ -6,14 +6,14 @@ $(document).ready(function(){
 
   function showLinks() {
     visibleLinks.forEach(function(e,index) {
-      $("#shiva_container_images ul").append("<li id='image_" + index + "'><a class='shiva_choose_image' href='#' id='clipboard_text_"+ index +"'>" + "<img src='" + e + "'/>" + "</a></li>");
+      $("#shiva_container_images ul").append("<li id='image_" + index + "'><a href='#' id='clipboard_text_"+ index +"'>" + "<img src='" + e + "'/>" + "</a><span class='shiva_text_clipboard' id ='clipboard_text_"+ index +"'>" + index + "</span></li>");
     });
   };  
 
 
   function populateSelect() {
     visibleLinks.forEach(function(e,index){
-      $("#shiva_select_image").append("<option value='"+ e +"'>" + index +"-"+ e +"</option>");
+      $("#shiva_select_image").append("<option value='"+ e +"'>Image " + index + "</option>");
     });
   };  
 
@@ -23,6 +23,7 @@ $(document).ready(function(){
     $("#shiva_container_partner").html(content.partner);
     $("#shiva_container_price").html(content.price);
     $("#shiva_container_url").html(content.url);
+    
     showLinks();
     populateSelect();
 
@@ -35,11 +36,26 @@ $(document).ready(function(){
                         function(activeTabs) {
         chrome.tabs.executeScript(activeTabs[0].id, {file: 'jquery.js'});
         chrome.tabs.executeScript(activeTabs[0].id, {file: 'content.js'});
-        $("#shiva_select_image").on("change", function(event){
-          console.log('The option with value ' + $(this).val() + ' and text ' + $(this).text() + ' was selected.');
-        });
+
+        //on change of the select value
+        $("#shiva_select_image").on("change", function(){
+          var str = "";
+          var selectValue = "";
+
+          $("#shiva_select_image option:selected").each(function () {
+            str += $(this).text() + " ";
+            selectValue += $(this).val() + " ";
+          });
+          
+
+          //debug which value am i getting?
+          // console.log('The option with value ' + selectValue + ' and text ' + str + ' was selected.');
+          
+          //print the value in the popu.html view
+          $("#shiva_container_image_path").html(selectValue);
+        })
+        .trigger('change');
       });
     });
   };
-
 });
